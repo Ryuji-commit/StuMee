dd9693e2a9b5009e17d5da4be82945032e7d9e08
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2020-05-21 13:20:10 +0900

    Change Meeting search form.

--- stumee_search/views.py
+++ stumee_search/views.py
@@ -6,7 +6,7 @@ from stumee_meeting.models import Thread
 
 # The ListView for search function of Meeting
 class MeetingSearchView(generic.ListView):
-    template_name = 'stumee_search/search_result.html'
+    template_name = 'stumee_search/meeting_search_result.html'
     context_object_name = 'search_results'
 
     def get_queryset(self):
