aa2c5ecceea944a2a839b4aee43acbb460d17864
Ryuji-commit <56810015+Ryuji-commit@users.noreply.github.com>
2020-07-01 18:33:49 +0900

    画面共有周りの実装

--- stumee_chat/static/stumee_chat/js/js_question.js
+++ stumee_chat/static/stumee_chat/js/js_question.js
@@ -10,49 +10,57 @@ let remoteVideos = document.getElementById('remote-videos');
 
 peer.on('open', function(){
     console.log('connected');
+    RoomLogin(false);
 });
 
 $('#start-video').on('click', function() {
-    RoomLogin();
+    existingRoom.close();
+    RoomLogin(true);
 });
 
-$('#end-video').on('click', function(room) {
+$('#end-video').on('click', function() {
     const localVideo = document.getElementById('local-video');
     if (localVideo != null){
         localVideo.srcObject.getTracks().forEach(track => track.stop());
         localVideo.srcObject = null;
+        LocalStream = null
     }
     existingRoom.close();
     setupMakeCallUI();
+    RoomLogin(false);
 });
 
-async function RoomLogin(){
-    const localVideo = document.getElementById('local-video');
-
-    LocalStream = await navigator.mediaDevices.getDisplayMedia({video: {width:800, height:600}})
-    $('#videos-container').show();
-    setupEndCallUI();
-
-    // render local video
-    localVideo.muted = true;
-    localVideo.srcObject = LocalStream;
-    localVideo.playsInline = true;
-    await localVideo.play().catch(console.error);
-
-    // logged in Room
+async function RoomLogin(is_video_flag){
     const RoomName = JSON.parse(document.getElementById('RoomNameURL').textContent);
-    room = peer.joinRoom(RoomName,{
-        mode: 'sfu',
-        stream: LocalStream,
-    });
+    if(is_video_flag == true){
+        const localVideo = document.getElementById('local-video');
+        LocalStream = await navigator.mediaDevices.getDisplayMedia({video: {width:1000, height:800} , audio: true})
+        $('#videos-container').show();
+        setupEndCallUI();
+
+        // render local video
+        localVideo.muted = true;
+        localVideo.srcObject = LocalStream;
+        localVideo.playsInline = true;
+        await localVideo.play().catch(console.error);
+
+        // logged in Room
+        room = peer.joinRoom(RoomName,{
+            mode: 'sfu',
+            stream: LocalStream,
+        });
+        console.log(room);
+    }else{
+        room = peer.joinRoom(RoomName,{
+            mode: 'sfu',
+            stream: null,
+        });
+    }
+    console.log(room);
     setupRoomEventHandlers(room);
 }
 
 function setupRoomEventHandlers(room){
-    if (existingRoom) {
-        existingRoom.close();
-    };
-
     existingRoom = room;
 
     room.on('stream', async function(stream){
@@ -61,9 +69,11 @@ function setupRoomEventHandlers(room){
 
         const newVideo = document.createElement('video');
         newVideo.srcObject = stream;
+        newVideo.muted = true;
         newVideo.playsInline = true;
         newVideo.setAttribute('data-peer-id', stream.peerId);
         $('#remote-videos').append(newVideo);
+        setUpUnMutedButton(stream.peerId);
         await newVideo.play().catch(console.error);
     });
 
@@ -74,10 +84,19 @@ function setupRoomEventHandlers(room){
     room.on('peerLeave', function(peerId){
         console.log(peerId + 'leaved');
         const remoteVideo = remoteVideos.querySelector('[data-peer-id="' + peerId + '"]');
+        const unMutedButton = document.querySelector('[id="' + peerId + '"]');
+        if (unMutedButton != null){
+            unMutedButton.remove();
+        }
+
         if (remoteVideo != null){
             remoteVideo.srcObject.getTracks().forEach(track => track.stop());
             remoteVideo.srcObject = null;
             remoteVideo.remove();
+        }
+
+        if (LocalStream == null){
+            removeAllRemoteVideos();
         }
     });
 
@@ -100,6 +119,25 @@ function setupEndCallUI() {
 function removeAllRemoteVideos(){
     $('#remote-videos').empty();
     $('#videos-container').hide();
+}
+
+function UnMuteRemoteVideo(video_id) {
+    const remoteVideo = remoteVideos.querySelector('[data-peer-id="' + video_id + '"]');
+    remoteVideo.muted = false;
+    console.log('un muted');
+    const unMutedButton = document.querySelector('[id="' + video_id + '"]');
+    unMutedButton.remove();
+}
+
+function setUpUnMutedButton(StreamPeerID){
+    const $remoteVideo = remoteVideos.querySelector('[data-peer-id="' + StreamPeerID + '"]');
+    const UnMutedButton = document.createElement('button');
+    UnMutedButton.setAttribute('type', 'submit');
+    UnMutedButton.setAttribute('class', 'btn btn-primary un-muted mb-2 align-top');
+    UnMutedButton.setAttribute('id', StreamPeerID);
+    UnMutedButton.setAttribute('onclick', "UnMuteRemoteVideo(this.id);");
+    UnMutedButton.innerHTML = '相手の音声を追加'
+    $remoteVideo.before(UnMutedButton);
 }
 
 $(function(){
